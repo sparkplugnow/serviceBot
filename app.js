52e9476691562ddf9a1b65f68b1d3795931a76c8
Israel Adura <israeladura@gmail.com>
2017-09-25 00:23:06 +0100

    first commit

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var Botkit = require('botkit');
-//var os = require('os');
+var os = require('os');
 //var komponist = require('komponist');
 //var request = require('request');
 //var sys = require('sys')
 //var exec = require('child_process').exec;
-var Token = require('./token.js');
+var token = require('./token.js');
 
 
-const playlistStore = [];
+//const playlistStore = []
 
 // Connect to slack
 
@@ -16,7 +16,7 @@ var controller = Botkit.slackbot({
 });
 
 var bot = controller.spawn({
-  token: Token.token
+  token: token.Sparkplug
 }).startRTM(function (err, bot, payload) {
   if (err) {
     console.log(err);
@@ -25,8 +25,8 @@ var bot = controller.spawn({
 });
 
 
-//var client = komponist.createConnection(6600, 'localhost', function () {
-//console.log('client created')
+// var client = komponist.createConnection(6600, 'localhost', function() {
+//   console.log('client created')
 //});
 
 
@@ -47,39 +47,35 @@ var knowledgebaseObject = {
 
   menuObject: {
 
-    Feedback: {},
-
     hoursOfOperation: ['Weekdays: 10am - 7pm | Weekends: 11am - 8pm'],
     faq: [
 
       {
-        question: 'What time are you open',
-        answer: ' Sample answer'
-      },
-      {
-        question: 'When will Donald Trump leave office',
-        answer: ' Sample answer'
-      },
-      {
-        question: 'What is the best kitchen in the world',
+        question: 'What time is  faq',
         answer: ' Sample answer'
       },
 
       {
-        question: 'how do i order food',
-        answer: ' Sample answer'
-      },
-      {
-        question: 'ETC... ETC...',
+        question: 'Sample faq',
         answer: ' Sample answer'
       }
     ],
-    contactInformation: {},
-
-    services: ['Cooking', 'Catering','Broiling']
+    services: ['service1', 'service 2', 'service 3']
   }
 };
 
+
+controller.hears(knowledgebaseObject.helpKeyword, ['direct_message', 'direct_mention', 'mention'], function (bot, message) {
+
+  bot.startConversation(message, function (err, convo) {
+
+    convo.addQuestion(knowledgebaseObject.optionList, function (response, convo) {
+      convo.say('you picked ' + response.text)
+
+    })
+  })
+
+});
 
 
 
@@ -89,7 +85,7 @@ controller.hears('hello', ['direct_message', 'direct_mention', 'mention'], funct
   // start a conversation to handle this response.
   bot.startConversation(message, function (err, convo) {
 
-    convo.addQuestion('Hi!, What is your name?', function (response, convo) {
+    convo.addQuestion('What is your name', function (response, convo) {
 
       convo.say('Cool, you said: ' + response.text);
       convo.say(knowledgebaseObject.welcomeMessage(response.text));
@@ -102,63 +98,131 @@ controller.hears('hello', ['direct_message', 'direct_mention', 'mention'], funct
     // convo.say(knowledgebaseObject.welcomeMessage);
     // convo.say(knowledgebaseObject.welcomeMessage);
 
+
   })
 
-});
-
-
-controller.hears('help', ['direct_message', 'direct_mention', 'mention'], function (bot, message) {
-
-  bot.startConversation(message, function (err, convo) {
-
-    convo.addQuestion(knowledgebaseObject.optionList, function (response, convo) {
-      convo.say('you picked ' + response.text);
-
-      //selectOption();
-      switch (response.text) {
-        //feedback option
-        case '1': bot.reply(message, 'Enter your feedback here please');
-          break;
-        //faq action
-        case '2': bot.reply(message, faq());
-          break;
-        //contact information action
-        case '3': bot.reply(message, 'getting contact information ');
-          break;
-        //service action
-        case '4': bot.reply(message,'we offer the following services \n'+ service());
-          break;
-        //call action
-        case '5': bot.reply(message, 'Hold on a bit, putting your call through.....');
-          break;
-        //Default action
-        default: bot.reply(message, 'kindly pick correct option');
-      }
-
-
-    })
-  })
 
 });
 
 
 
 
-//FUNCTIONAL UNITS
 
-//list out faqs
-var faq = function () {
-  var faqs = (knowledgebaseObject.menuObject.faq).map(function (faq, idx) {
-    var list = (idx + 1, ':', faq.question);
-    return list
-  }).join('\n');
-  return faqs;
-}
+controller.hears('list', ['direct_message', 'direct_mention', 'mention'], function (bot, message) {
 
-//list out services
-var service = function () {
-  var services = (knowledgebaseObject.menuObject.services).map(function (service, idx) {
-    return (idx + 1, ':', service)
-  }).join('\n');
-  return services;
-}
+  str = message.text;
+  client.playlist(function (err, info) {
+
+    info = info.toString();
+
+    var reply_with_attachments = {
+
+      'attachments': [{
+        'fallback': 'List of songs in Playlist',
+        'title': 'Playlist',
+        'text': info,
+        "fields": [{
+            "Song 1": 'test',
+
+            "short": false
+          }
+
+
+        ],
+
+        'color': '#7CD197'
+      }],
+      'icon_url': 'http://southpawgroup.com/gidiloungeart/images/albums_thumbnail/Untitled.jpg'
+    }
+
+
+    bot.reply(message, reply_with_attachments);
+
+
+  })
+
+
+
+
+})
+
+//   client.currentsong(function(err, info) {
+//         console.log(info.Artist); 
+//         console.log(info.Title);  
+//         console.log(info.Album);  
+//         var nowPlaying = info.file
+//         nowPlaying = nowPlaying.replace(/.mp3/g, '');
+//         var reply_with_attachments = {
+
+// 'attachments': [
+//   {
+//     'fallback': 'Now Playing attachments',
+//     'title': 'Now Playing' + '\n' + 'Stream link - http://158.85.113.45:7500',
+
+//       "fields": [
+//             {
+//                 "title": nowPlaying,
+
+//                 "short": false
+//             }
+//         ],
+
+//     'color': '#7CD197'
+//   }
+// ],
+// 'icon_url': 'http://southpawgroup.com/gidiloungeart/images/albums_thumbnail/Untitled.jpg'
+// }
+// });
+
+// MPD playlist
+//        client.playlist(function(err, info) {
+//          console.log(info)
+//        })
+
+
+
+
+// //search request
+// controller.hears('search',['direct_message', 'direct_mention', 'mention'], function(bot, message) {
+
+// var str = message.text;
+
+// //remove search keyword
+// var newStr = str.replace(/search/i, '');
+
+// //remove whitespace
+// newStr = newStr.replace(/ /i,'');
+// console.log(newStr);
+
+// var bashCommand = './runyoutube.sh ' + '\'' + newStr + '\'';
+
+// dir = exec(bashCommand, function(err, stdout, stderr) {
+//   if (err) {
+//     // should have err.code here?  
+//   }
+//   //bot.reply(message, stdout )
+//   console.log(stdout);
+// });
+
+// dir.on('exit', function (code) {
+//   // exit code is code
+//   bot.reply(message, 'Song Added to Queue' )
+// });
+
+
+// //bot.reply(message, newStr )
+
+
+//   var sendSearch = function(searchterm){
+
+//             var options = {
+
+//               url: 'http://gplayer.herokuapp.com/api/search?q='+searchterm,
+//               headers: {
+//                   'Accept': 'application/json',
+//                   'Content-Type': 'application/json',                  
+//             }
+//           }
+//             console.log('making network request')  
+//  }
+// })
